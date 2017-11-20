425d851c4c0d2cbac67345bda439485371612207
creag0 <wcreager7@gmail.com>
2017-11-20 17:17:45 -0500

    update win_agent.rb to include path var's

--- attributes/win_agent.rb
+++ attributes/win_agent.rb
@@ -1,3 +1,5 @@
 
 
 default['zabbix_ng']['win_agent']['source'] ='http://www.zabbix.com/downloads/3.0.4/zabbix_agents_3.0.4.win.zip'
+default['zabbix_ng']['win_agent']['inst_media'] = 'C:\\zabbix_agents_3.0.4.win.zip'
+default['zabbix_ng']['win_agent']['inst_dir'] = 'C:\\zabbix_agents_3.0.4.win'
--- recipes/win_agent.rb
+++ recipes/win_agent.rb
@@ -8,24 +8,25 @@ include_recipe "seven_zip"
 
 
 # Download Zabbix zip file
-remote_file 'C:\\zabbix_agents_3.0.4.win.zip' do
+remote_file node['zabbix_ng']['win_agent']['inst_media'] do
 	source node['zabbix_ng']['win_agent']['source']
 	action :create
 end
 
 # Unzip Zabbix Agent media
 seven_zip_archive 'zabbix_agent_source' do
-  path      'C:\\zabbix_agents_3.0.4.win'
-  source    'C:\\zabbix_agents_3.0.4.win.zip'
+  path      ['zabbix_ng']['win_agent']['inst_dir']
+  source    node['zabbix_ng']['win_agent']['inst_media']
   overwrite true
   timeout   30
-  not_if	{::File.exists?('C:\\zabbix_agents_3.0.4.win')}
+  #not_if	{::File.exists?('C:\\zabbix_agents_3.0.4.win')}
+	not_if	{::File.exists?node['zabbix_ng']['win_agent']['inst_dir']}
 end
 
 # Install Zabbix Agent
 execute 'zabbix-agent' do
-	command 'C:\\zabbix_agents_3.0.4.win\\bin\\win64\\zabbix_agentd.exe --config C:\\zabbix_agents_3.0.4.win\\conf\\zabbix_agentd.win.conf --install'
-  #not_if  {::File.exists?('C:\\zabbix_agents_3.0.4.win')}
+	#command 'C:\\zabbix_agents_3.0.4.win\\bin\\win64\\zabbix_agentd.exe --config C:\\zabbix_agents_3.0.4.win\\conf\\zabbix_agentd.win.conf --install'
+	command 'node['zabbix_ng']['win_agent']['inst_dir']\\bin\\win64\\zabbix_agentd.exe --config C:\\zabbix_agents_3.0.4.win\\conf\\zabbix_agentd.win.conf --install'
+	#not_if  {::File.exists?('C:\\zabbix_agents_3.0.4.win')}
   	not_if 'sc query "Zabbix Agent" | find "RUNNING"'
 end
-

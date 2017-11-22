c1df9cf0b842567e7e5823c7aac86ac67e2f0bb0
creag0 <wcreager7@gmail.com>
2017-11-22 16:31:42 -0500

    cleaned up comments for not_if in win_agent.rb

--- recipes/win_agent.rb
+++ recipes/win_agent.rb
@@ -1,9 +1,5 @@
 
-#####
-# Still must fix hard coded paths!
-#####S
 
-# make this work with seven_zip coobook
 include_recipe "seven_zip"
 
 
@@ -19,14 +15,11 @@ seven_zip_archive 'zabbix_agent_source' do
   source    node['zabbix_ng']['win_agent']['inst_media']
   overwrite true
   timeout   30
-  #not_if	{::File.exists?('C:\\zabbix_agents_3.0.4.win')}
 	not_if	"::File.exists?#{node['zabbix_ng']['win_agent']['inst_dir']}"
 end
 
 # Install Zabbix Agent
 execute 'zabbix-agent' do
-	#command 'C:\\zabbix_agents_3.0.4.win\\bin\\win64\\zabbix_agentd.exe --config C:\\zabbix_agents_3.0.4.win\\conf\\zabbix_agentd.win.conf --install'
 	command node['zabbix_ng']['win_agent']['inst_dir'] + '\\bin\\win64\\zabbix_agentd.exe --config C:\\zabbix_agents_3.0.4.win\\conf\\zabbix_agentd.win.conf --install'
-	#not_if  {::File.exists?('C:\\zabbix_agents_3.0.4.win')}
   	not_if 'sc query "Zabbix Agent" | find "RUNNING"'
 end
